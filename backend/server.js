19dd501f21aa89dd473927a266eb7b82a562442b
Christine Dy <20230028712@my.xu.edu.ph>
2026-04-18 09:44:52 -0400

    added application route

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,20 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const mongoose = require("mongoose");
-
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log ("MongoDB Connected"))
+    .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
-    
+
 app.use(cors());
 app.use(express.json());
+
+//routes
+const applicationsRoute = require('./routes/applications');
+app.use('/api/applications', applicationsRoute);
 
 app.get("/", (req, res) => {
     res.send("Backend Running");
