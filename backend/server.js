040cc63cfe6f39aabf245d6259b2eb098e218fd5
Elisha Cogtas <20230028752@my.xu.edu.ph>
2026-05-23 12:52:02 +0800

    backup every 12 mindnight

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI)
             console.error("[AUTOMATION] Startup backup test encountered an error:", testError);
         }
         
-        cron.schedule('50 12 * * *', async () => {
+        cron.schedule('0 0 * * *', async () => {
             console.log("[AUTOMATION] Midnight clock struck! Initiating automatic cloud backup to Google Drive...");
             try {
                 await executeSystemBackup();
