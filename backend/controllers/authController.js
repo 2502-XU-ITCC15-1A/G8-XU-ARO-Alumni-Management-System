4a2797b930457a30df263c98586197496ba4efe3
Elisha Cogtas <20230028752@my.xu.edu.ph>
2026-05-25 04:44:10 +0800

    forgot-reset-pass issue (3)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -216,8 +216,7 @@ if (!user) {
 
     const oauth2Client = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_CLIENT_SECRET,
-      process.env.GOOGLE_REDIRECT_URI
+      process.env.GOOGLE_CLIENT_SECRET
     );
 
     oauth2Client.setCredentials({
